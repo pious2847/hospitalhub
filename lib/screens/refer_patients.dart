5073395a54272f72bfa875fc0f39810e6301720e
pious <109950629+pious2847@users.noreply.github.com>
2024-09-16 16:15:05 +0000

    adding refer_patients

---
+++ lib/screens/refer_patients.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReferPatients extends StatefulWidget {
+  const ReferPatients({super.key});
+
+  @override
+  State<ReferPatients> createState() => _ReferPatientsState();
+}
+
+class _ReferPatientsState extends State<ReferPatients> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
